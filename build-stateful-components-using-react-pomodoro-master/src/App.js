fe4807e6af9ea21a288f9a6447a40cff3f1a135d
masayuki <masayuki.inoue@woven-planet.global>
2022-08-28 11:43:53 +0900

    Setup week2 practice

---
+++ build-stateful-components-using-react-pomodoro-master/src/App.js
@@ -0,0 +1,29 @@
+// Uncomment the import statemnets while running the test code and while submitting the solution
+// Comment this code while running the solution in the browser
+import React from 'react';
+
+
+//Write the code for creating App class component
+class App extends React.Component{
+
+    /* define the constructor and initialize the following:
+       1. define timer states with values 'Running', 'Stopped' and 'Ended'
+       2. define timers object with object properties: pomodoro and shortBreak, where each property 
+       object should have properties: type, timeout, timerState, timeLeft, timeLeftDisplay and message
+       3. set the 'currentTimer' state value to pomodoro that has been defined in 2nd step 
+    */
+    // define startTimer() function to start timer and update currentTimer state
+    
+    // define endTimer() function to end current timer and navigate to next timer
+    
+    // define navigateToTimer() function to update currentTimer state with given timer
+
+    // define navigateToNextTimer() function to update currentTimer with next timer state
+    
+    // define stopTimer() function to pause the current timer and update the currentTimer state
+}
+
+
+// uncomment the export statement while testing the code and submitting the solution
+// comment this code while running the solution in the browser
+module.exports={App};
