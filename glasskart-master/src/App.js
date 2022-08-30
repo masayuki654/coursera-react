1ac9b5f7b30bb9f6cbeebad453d148f6749adead
masayuki <masayuki.inoue@woven-planet.global>
2022-08-30 19:28:24 +0900

    Complete glasskart-master

--- glasskart-master/src/App.js
+++ glasskart-master/src/App.js
@@ -4,9 +4,34 @@ import React from 'react';
 
 
 //Write the code for creating Product function component that returns a React element
+function Product(props) {
+    if (props.outOfStock) {
+        return null;
+    }
+    return React.createElement("div", null,
+           React.createElement("img", {src: props.imageSrc, width: "150px", height: "150px"}, null),
+           React.createElement("div", {style: {"fontFamily": "serif"}}, props.productName), !props.discountPrice ?
+           React.createElement("div", {style: {"fontFamily": "serif"}}, props.price) :
+           React.createElement("div", {style: {"display": "flex"}},
+           React.createElement("div", {style: {"fontFamily": "serif"}}, React.createElement("s", null, props.price)),
+           React.createElement("div", {style: {"fontFamily": "serif"}}, props.discountPrice)) 
+           );
+}
 
 //Write the code for creating ProductList function component that returns a React element
-
+function ProductList(props) {
+    const productList = props.products;
+    return productList.map(product => {
+        return React.createElement(Product, {
+            key: product.productName,
+            productName: product.productName,
+            outOfStock: product.outOfStock,
+            discountPrice: product.discountPrice,
+            price: product.price,
+            imageSrc: product.imageSrc
+        });
+    });
+}
 
 // uncomment the export statement while testing the code and submitting the solution
 // comment this code while running the solution in the browser
